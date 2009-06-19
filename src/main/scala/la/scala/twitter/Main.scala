8e364dce3331c7f1790a5928b6ce921f1e956c35
Alexy Khrabrov <deliverable@gmail.com>
2009-06-19 02:29:26 -0400

    extract status works indeed

--- src/main/scala/la/scala/twitter/Main.scala
+++ src/main/scala/la/scala/twitter/Main.scala
@@ -1,15 +1,57 @@
-package la.scala.twitter
+package com.tfitter
 
 import org.talkingpuffin.twitter._
+import la.scala.twitter.db._
+import _root_.scala.io._
 
-object Main extends Application {
-  val twitterUser = "khrabrov"
-  val twitterPassword = readLine
-
-  val auth = TwitterSession(twitterUser,twitterPassword)
-
-  val tweets = auth.getUserTimeline(twitterUser)
+object Main {
+  // Access would have to be compiled after filtering,
+  // http://la.scala.la/post/124159629/encrypting-and-replacing-passwords-with-maven
+  // import Access.{twitterUser, twitterPassword}
   
-  for (t <- tweets)
-    println(t)
+  def queryTwitter(twitterUser: String, twitterPassword: String) {
+    val auth = TwitterSession(twitterUser,twitterPassword)
+    val tweets = auth.getUserTimeline(twitterUser)
+    for (t <- tweets) println(t)
+  }
+  
+  def doPGtest {
+    val tdb = new TwitPG("jdbc:postgresql:twitter","alexyk","","testRange")
+      
+    tdb.testRange
+  }
+  
+  def propertyTest = {
+    // val dbDriver = Class.forName("org.postgresql.Driver")
+    // doPGtest
+    
+    def getProperties = this.getClass getResourceAsStream "/application.properties"
+    var st = getProperties
+          
+    // in order to reset, do ss = ss.reset // !
+    var ss = BufferedSource.fromInputStream(st, "UTF-8", 512, { 
+      () =>  Source.fromInputStream(getProperties) })
+      
+    val linesIter = ss.getLines
+    val twitterUser=linesIter.next.trim
+    // can use some simple steganography here
+    val twitterPassword=linesIter.next.trim
+    
+    println("user=>"+twitterUser+", password=>"+twitterPassword)
+  }
+  
+  /*
+  def testBdb = {
+    import la.scala.util.bdb.Env
+    
+    val env = new Env(new File(".")) with Transactional with AllowCreate
+    env { e =>
+        println(e)
+    }
+  }
+  */
+
+  def main(args: Array[String]) = {
+    println("eww, world!")
+  }
 }
