96f9e76c5454a33b510a0f23017415c4900c872b
Alexy Khrabrov <deliverable@gmail.com>
2009-07-31 00:07:43 -0400

    try to serialize HashMaps, i.e. store adjacency graph in BDB

--- src/main/scala/com/tfitter/db/reps-bdb.scala
+++ src/main/scala/com/tfitter/db/reps-bdb.scala
@@ -10,10 +10,13 @@ import org.suffix.util.bdb.{BdbArgs,BdbFlags,BdbStore}
 import com.sleepycat.persist.model.{Entity,PrimaryKey,SecondaryKey}
 import com.sleepycat.persist.model.Relationship.MANY_TO_ONE
 import scala.collection.mutable.{Map=>UMap}
-import java.util.{HashMap=>JHashMap}
+
+import java.lang.{Integer=>JInt}
+import java.util.{HashMap=>JHMap}
 
 object types {
   type RepCount = UMap[UserID,(TwitCount,TwitCount)]
+  type ReplierMap = UMap[Int,RepCount]
   type FixedRepliers = Map[UserID, Map[UserID,(TwitCount,TwitCount)]]
 }
 import types._
@@ -25,19 +28,24 @@ case class RepPair (
   reps: TwitCount,
   dirs: TwitCount
   )
+  
+case class UserReps (
+  s: UserID,
+  rc: RepCount
+  )
    
 @Entity
 class RepPairBDB {
   // or can just shift two Ints into a Long
   @PrimaryKey
   // var st: java.util.AbstractMap.SimpleImmutableEntry[java.lang.Integer,java.lang.Integer] = null
-  var st: java.lang.Long = null
+  var st: JInt = null
   @SecondaryKey{val relate=MANY_TO_ONE}
-  var s: java.lang.Integer = null
+  var s: JInt = null
   @SecondaryKey{val relate=MANY_TO_ONE}
-  var t: java.lang.Integer = null
-  var reps: java.lang.Integer = null
-  var dirs: java.lang.Integer = null
+  var t: JInt = null
+  var reps: JInt = null
+  var dirs: JInt = null
   def this(_s: UserID, _t: UserID, _reps: TwitCount, _dirs: TwitCount) = { 
     this()
     // st = new java.util.AbstractMap.SimpleImmutableEntry(_s, _t)
@@ -60,14 +68,35 @@ class RepPairBDB {
 }
 
 @Entity
-class UserRepliersBDB {
+class UserRepsBDB {
   @PrimaryKey
   var s: java.lang.Integer = null
-  var t: JHashMap[java.lang.Integer,java.lang.Integer] = null
-  var u: JHashMap[java.lang.Integer,java.lang.Integer] = null
-  def this(_s: UserID, r: RepCount) = { this()
+  var t: JHMap[JInt,JInt] = null
+  var u: JHMap[JInt,JInt] = null
+  def this(_s: UserID, rc: RepCount) = { this()
+  // Map(1->(2,3),4->(5,2),2->(7,8)).foldLeft(Nil:List[(Int,Int)],Nil:List[(Int,Int)]) { 
+  // case ((acc1, acc2), (x, (y, z))) => ( (x,y)::acc1, (x,z)::acc2) }  
     s = _s
-    // do maps here     
+    t = new JHMap
+    u = new JHMap
+    rc.foreach { case (x, (y, z)) => t.put(x,y); u.put(x,z) }    
+  }
+  def toUserReps: UserReps = {
+    val sid = s.intValue
+    var m: RepCount = UMap.empty
+    val tit = t.entrySet.iterator
+    val uit = u.entrySet.iterator
+    while (tit.hasNext) {
+      val tp = tit.next
+      val up = uit.next
+      val s = tp.getKey
+      assert (up.getKey == s)
+      val t = tp.getValue
+      val u = up.getValue
+      // m += (s -> (t,u))
+      m(s.intValue) = (t.intValue,u.intValue)
+    }
+    UserReps(sid,m)
   }
 }
 
@@ -188,6 +217,82 @@ object FetchRepliersBDB extends optional.Application {
     
     err.print("Loading Repliers from Berkeley DB... ")
     val reps: FixedRepliers = repsDb.loadMap(showingProgress)
+    err.println("done")
+  }
+}
+
+
+class RepMapsBDB(bdbArgs: BdbArgs) extends BdbStore(bdbArgs) {
+  val urPrimaryIndex =
+    store.getPrimaryIndex(classOf[JInt], classOf[UserRepsBDB])
+      
+  def saveMap(r: Repliers, showProgress: Boolean): Unit = {
+    var userCount = 0
+    for ((s,rc) <- r.reps) {
+      val userReps = new UserRepsBDB(s,rc)
+      urPrimaryIndex.put(txn, userReps)
+      userCount += 1
+      if (showProgress && userCount % 100000 == 0) err.print('.')
+    }
+    err.println
+  }
+  
+  def loadMap(showProgress: Boolean): ReplierMap = { 
+    val curIter = new CursorIterator(urPrimaryIndex.entities)
+    var reps: ReplierMap = UMap.empty
+
+    var userCount = 0
+    for (uj <- curIter) {
+      val ur: UserReps = uj.toUserReps
+      reps(ur.s) = ur.rc
+      userCount += 1
+      if (showProgress && userCount % 100000 == 0) err.print('.')
+    }
+    err.println
+    reps
+  }
+}
+
+
+object StoreUserRepsBDB extends optional.Application {
+  def main(
+    envName: Option[String],
+    storeName: Option[String],
+    cacheSize: Option[Double],
+    allowCreate: Option[Boolean],
+    readOnly: Option[Boolean],
+    transactional: Option[Boolean],
+    deferredWrite: Option[Boolean],
+    noSync: Option[Boolean],
+    showProgress: Option[Boolean],
+    args: Array[String]) = {
+      
+    val repSerName: String = args(0) // need it
+
+    val bdbEnvPath   = envName getOrElse "urs.bdb"// Config.bdbEnvPath
+    val bdbStoreName = storeName getOrElse "repmaps"// Config.bdbStoreName
+    val bdbCacheSize = cacheSize match {
+      case Some(x) => Some((x*1024*1024*1024).toLong)
+      case _ => None // Config.bdbCacheSize
+    }
+    val bdbFlags = BdbFlags(
+      allowCreate   getOrElse false,
+      readOnly      getOrElse false,
+      transactional getOrElse false,
+      deferredWrite getOrElse false,
+      noSync        getOrElse false
+    )
+    val bdbArgs = BdbArgs(bdbEnvPath,bdbStoreName,bdbFlags,bdbCacheSize)
+
+    // make this a parameter:
+    val showingProgress = showProgress getOrElse true
+
+    val ursDb = new RepMapsBDB(bdbArgs)
+    
+    val reps: Repliers = loadRepliers(repSerName)
+    
+    err.print("Saving Repliers to Berkeley DB... ")
+    ursDb.saveMap(reps,showingProgress)
     err.println("done")
   }
 }
