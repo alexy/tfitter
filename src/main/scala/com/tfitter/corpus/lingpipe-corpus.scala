220f11a96d99485ac31c8c637b80d79ab69b06d1
Alexy Khrabrov <deliverable@gmail.com>
2009-08-14 13:35:47 -0400

    top bigrams, parameter

--- src/main/scala/com/tfitter/corpus/lingpipe-corpus.scala
+++ src/main/scala/com/tfitter/corpus/lingpipe-corpus.scala
@@ -1,17 +1,19 @@
 package com.tfitter.corpus
 
-import aliasi.util.ScoredObject
+import aliasi.tokenizer._
+import java.util.regex.Pattern
+import com.aliasi.util.ScoredObject
 import System.err
-import aliasi.tokenizer.{IndoEuropeanTokenizerFactory, TokenizerFactory}
 import com.aliasi.corpus.{TextHandler,Corpus}
 import com.aliasi.lm.TokenizedLM
 import db.{TwitterDB, TwitterBDB, TwIterator}
 
+// java.util.SortedSet.toList
 import scala.collection.jcl.Conversions._
 
 import org.suffix.util.bdb.{BdbFlags, BdbArgs}
-class TwitterCorpus(bdbArgs: BdbArgs) extends Corpus[TokenizedLM] {
-  def visitTest(handler: TextHandler): Unit = {}
+class TwitterCorpus(bdbArgs: BdbArgs) extends Corpus[TextHandler] {
+  override def visitTest(handler: TextHandler): Unit = {}
 
   // Iterator.take(Int) -- not Long, unacceptable generally!
   private var maxTwits: Option[Int] = None
@@ -22,18 +24,18 @@ class TwitterCorpus(bdbArgs: BdbArgs) extends Corpus[TokenizedLM] {
   def setTwitsProgress(m: Int): Unit = twitsProgress = Some(m)
   def unsetTwitsProgress: Unit = twitsProgress = None
 
-  override def visitTrain(lm: TokenizedLM): Unit = {
+  override def visitTrain(handler: TextHandler): Unit = {
     val twits: TwIterator = tdb.allTwits
     val twitsToGo = maxTwits match {
       case Some(m) => twits.take(m)
       case _ => twits
     }
-    err.println("got twits "+twitsToGo.hasNext)
+    if (twitsToGo.hasNext) err.println("got twits")
     // get the total here as it's "remaining"!
     var totalTwits = 0
     for (t <- twitsToGo) {
       // err.println(totalTwits+": "+t.text)
-      lm.handle(t.text.toCharArray,0,t.text.length)
+      handler.handle(t.text.toCharArray,0,t.text.length)
       totalTwits += 1
       if (!twitsProgress.isEmpty && totalTwits % twitsProgress.get == 0) err.print(".")
     }
@@ -56,9 +58,15 @@ object Pipe extends optional.Application {
     noSync: Option[Boolean],
     maxTwits: Option[Int],
     showProgress: Option[Int],
-    gram: Int,
+    gram: Option[Int],
+    topGram: Option[Int],
+    top: Option[Int],
     args: Array[String]) = {
 
+    val gram_ = gram getOrElse 2
+    val topGram_ = topGram getOrElse 2
+    val top_ = top getOrElse 20
+    
     val bdbEnvPath   = envName getOrElse "bdb"
     val bdbStoreName = storeName getOrElse "twitter"
 
@@ -79,16 +87,25 @@ object Pipe extends optional.Application {
     if (!showProgress.isEmpty) twitCorpus.setTwitsProgress(showProgress.get)
     twitCorpus.setMaxTwits(maxTwits getOrElse 100)
 
-    val tf: TokenizerFactory = IndoEuropeanTokenizerFactory.INSTANCE
-    val lm = new TokenizedLM(tf,gram)
+    val tf: TokenizerFactory = twitTokenizerFactory
+    val lm = new TokenizedLM(tf,gram_)
 
     twitCorpus.visitTrain(lm)
 
-    val freqTerms: List[ScoredObject[Array[String]]] = lm.frequentTermSet(1, 10).toList
+    val freqTerms: List[ScoredObject[Array[String]]] = lm.frequentTermSet(topGram_, top_).toList
 
     for (so <- freqTerms) {
       println(so.score+": "+so.getObject.toList.mkString(","))
     }
 
   }
+
+  def twitTokenizerFactory: TokenizerFactory = {
+      var factory = IndoEuropeanTokenizerFactory.INSTANCE
+      factory = new RegExFilteredTokenizerFactory(factory,Pattern.compile("\\p{Alpha}+"))
+      factory = new LowerCaseTokenizerFactory(factory)
+      factory = new EnglishStopTokenizerFactory(factory)
+      factory
+  }
+
 }
